5d8aa44ac0952a58b4296ce618b7a4e3a5659f83
sirlestr <sir.lestr@gmail.com>
2024-03-18 09:21:55 +0100

    Add project files.

---
+++ Models/People.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudApp.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Jméno")]
+        public  string FirstName { get; set; }
+
+        [Display(Name = "Příjmení")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Je aktivní?")]
+        public bool IsActive { get; set; }
+
+
+    }
+}
